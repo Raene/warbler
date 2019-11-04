addcfade013905ff8b483c1f13065e69c29251cc
Raene <crofty.52.bu@gmail.com>
2019-11-04 10:16:24 +0100

    Code Cleanup Addition of errorMiddleware Addition of Test route for errorMiddleware

--- backend-api/index.js
+++ backend-api/index.js
@@ -3,16 +3,35 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
-var messageRoutes = require("./routes/messages");
-var userRoutes = require("./routes/users");
-var todoroutes = require("./routes/todos");
+const messageRoutes = require("./routes/messages");
+const userRoutes = require("./routes/users");
+const todoroutes = require("./routes/todos");
+const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
+const { ErrorHandler } = require("./helpers/error");
+
+const { errorMiddleware } = require("./middleware/error");
 
 app.disable("x-powered-by");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api/users/:id/messages", messageRoutes);
+app.use(
+  "/api/users/:id/messages",
+  loginRequired,
+  ensureCorrectUser,
+  messageRoutes
+);
 app.use("/api/users/auth", userRoutes);
 app.use("/api/todo", todoroutes);
+
+//test route for error middleware
+app.get("/error", (req, res, next) => {
+  let err = new ErrorHandler();
+
+  next(err);
+});
+
+//error middleware must get called last for stable performance
+app.use(errorMiddleware);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
