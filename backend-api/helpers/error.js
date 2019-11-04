1c88b371a4c73a4151fa6ab4aec206651a7c5a8b
Raene <crofty.52.bu@gmail.com>
2019-11-04 10:16:42 +0100

    error heler created

---
+++ backend-api/helpers/error.js
@@ -0,0 +1,19 @@
+class ErrorHandler extends Error {
+  constructor(statusCode, message) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+  }
+
+  displayError(res) {
+    res.status(this.statusCode || 500).json({
+      status: "error",
+      statusCode: this.statusCode || 500,
+      message: this.message || "Something went wrong man"
+    });
+  }
+}
+
+module.exports = {
+  ErrorHandler
+};
