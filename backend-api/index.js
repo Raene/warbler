ce68f4c9d48b839083d1b23353a2becddb846485
Raene <crofty.52.bu@gmail.com>
2019-11-07 21:32:09 +0100

    added api to fetch all messages Move to message handler later

--- backend-api/index.js
+++ backend-api/index.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const messageRoutes = require("./routes/messages");
 const userRoutes = require("./routes/users");
 const todoroutes = require("./routes/todos");
+const db = require("./models");
 const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
 const { ErrorHandler } = require("./helpers/error");
 
@@ -23,6 +24,18 @@ app.use(
 );
 app.use("/api/users/auth", userRoutes);
 app.use("/api/todo", todoroutes);
+
+app.get("/api/message", async function(req, res, next) {
+  try {
+    let messages = await db.Message.find()
+      .sort({ createdAt: "desc" })
+      .populate("user", {
+        name: true,
+        profileImageUrl: true
+      });
+    return res.status(200).json(messages);
+  } catch (err) {}
+});
 
 //test route for error middleware
 app.get("/error", (req, res, next) => {
